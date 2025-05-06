b9590d9f969ec43dc356fbff2afa0eb21b085973
caiobinharj <caioasc886@gmail.com>
2025-05-06 10:55:49 -0300

    Initialize project using Create React App

--- src/Posts.js
+++ src/Posts.js
@@ -1,77 +1,92 @@
-export default function Body(){
+import React from "react";
+
+export default function Posts(){
+
+    const [posts, setPosts] = React.useState([
+        {img1:"meowed", img2:"gato-telefone", img3:"respondeai", curtidas:12031, gostado: false, salvo: false, id:0},
+        {img1:"barked", img2:"dog", img3:"adorable_animals", curtidas:43213, gostado: false, salvo: false, id:1},
+        {img1:"dog", img2:"adorable_animals", img3:"adorable_animals", curtidas:123124, gostado: false, salvo: false, id:2},
+        {img1:"adorable_animals", img2:"meowed", img3:"meowed", curtidas:12312432, gostado: false, salvo: false, id:3},
+        {img1:"barked", img2:"barked", img3:"adorable_animals", curtidas:99323, gostado: false, salvo: false, id:4}
+
+    ]);
+
+    function handleCurtir(id, isToggle = false) {
+        setPosts(posts.map(post => {
+            if (post.id === id) {
+                if (!isToggle && post.gostado) return post;
+
+                const novoEstado = isToggle ? !post.gostado : true;
+                return {
+                    ...post,
+                    curtidas: novoEstado ? post.curtidas + 1 : post.curtidas - 1,
+                    gostado: novoEstado
+                };
+            }
+            return post;
+        }));
+    }
+
+    function toggleSalvo(id) {
+        setPosts(posts.map(post =>
+            post.id === id ? { ...post, salvo: !post.salvo } : post
+        ));
+    }
+
+    const printPosts = posts.map((post)=> {
+
+        return(
+            <div className="post" key={post.id}>
+            <div className="topo">
+                <div className="usuario">
+                    <img src={`${post.img1}.svg`}
+                         alt={post.img1}/>
+                    meowed
+                </div>
+                <div className="acoes">
+                    <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
+            </div>
+
+                <div className="conteudo" onClick={() => handleCurtir(post.id)}>
+                    <img src={`${post.img2}.svg`} alt={post.img2}/>
+                </div>
+
+                <div className="fundo">
+                    <div className="acoes">
+                        <div>
+                        <ion-icon
+                                name={post.gostado ? "heart" : "heart-outline"}
+                                style={{color: post.gostado ? "red" : "inherit"}}
+                                onClick={() => handleCurtir(post.id, true)}
+                            />
+                            <ion-icon name="chatbubble-outline"></ion-icon>
+                            <ion-icon name="paper-plane-outline"></ion-icon>
+                        </div>
+                    <div>
+                        <ion-icon
+                            name={post.salvo ? "bookmark" : "bookmark-outline"}
+                            onClick={() => toggleSalvo(post.id)}
+                        />
+                    </div>
+                </div>
+
+                <div className="curtidas">
+                    <img
+                        src={`${post.img3}.svg`}
+                        alt={post.img3}/>
+                    <div className="texto">
+                        Curtido por <strong>{post.img3}</strong> e <strong>outras {post.curtidas} pessoas</strong>
+                    </div>
+                </div>
+            </div>
+        </div>)
+
+    })
     return (
         <div className="posts">
-            <div className="post">
-                <div className="topo">
-                    <div className="usuario">
-                        <img src="../assets/img/meowed.svg" alt="meowed"/>
-                        meowed
-                    </div>
-                    <div className="acoes">
-                        <ion-icon name="ellipsis-horizontal"></ion-icon>
-                    </div>
-                </div>
+            {printPosts}
 
-                <div className="conteudo">
-                    <img src="../assets/img/gato-telefone.svg" alt="gato-telefone"/>
-                </div>
-
-                <div className="fundo">
-                    <div className="acoes">
-                        <div>
-                            <ion-icon name="heart-outline"></ion-icon>
-                            <ion-icon name="chatbubble-outline"></ion-icon>
-                            <ion-icon name="paper-plane-outline"></ion-icon>
-                        </div>
-                        <div>
-                            <ion-icon name="bookmark-outline"></ion-icon>
-                        </div>
-                    </div>
-
-                    <div className="curtidas">
-                        <img src="../assets/img/respondeai.svg" alt="respondeai"/>
-                        <div className="texto">
-                            Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="post">
-                <div className="topo">
-                    <div className="usuario">
-                        <img src="../assets/img/barked.svg" alt="barked"/>
-                        barked
-                    </div>
-                    <div className="acoes">
-                        <ion-icon name="ellipsis-horizontal"></ion-icon>
-                    </div>
-                </div>
-
-                <div className="conteudo">
-                    <img src="../assets/img/dog.svg" alt="dog"/>
-                </div>
-
-                <div className="fundo">
-                    <div className="acoes">
-                        <div>
-                            <ion-icon name="heart-outline"></ion-icon>
-                            <ion-icon name="chatbubble-outline"></ion-icon>
-                            <ion-icon name="paper-plane-outline"></ion-icon>
-                        </div>
-                        <div>
-                            <ion-icon name="bookmark-outline"></ion-icon>
-                        </div>
-                    </div>
-
-                    <div className="curtidas">
-                        <img src="../assets/img/adorable_animals.svg" alt="adorable_animals"/>
-                        <div className="texto">
-                            Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     )
 }
--- src/Stories.js
+++ src/Stories.js
@@ -1,78 +1,24 @@
-export default function Body(){
+export default function Stories(){
+    let listaNomes = ["9gag", "meowed", "barked", "nathanwpylestrangeplanet", "wawawicomics", "respondeai", "filomoderna", "memeriagourmet"];
+
+    const listaStories = listaNomes.map(nome => {
+        let imagem = `${nome}.svg`;
+        return(<div className="story" key={nome}>
+            <div className="imagem">
+                <img src={imagem} alt={nome}/>
+            </div>
+            <div className="usuario">
+                {nome}
+            </div>
+        </div>)
+
+    })
     return (
         <div className="stories">
-            <div className="story">
-                <div className="imagem">
-                    <img src="../assets/img/9gag.svg" alt="9gag"/>
-                </div>
-                <div className="usuario">
-                    9gag
-                </div>
+            {listaStories}
+            <div className="setinha">
+                <ion-icon name="chevron-forward-circle"></ion-icon>
             </div>
-
-            <div className="story">
-                <div className="imagem">
-                    <img src="../assets/img/meowed.svg" alt="meowed"/>
-                </div>
-                <div className="usuario">
-                    meowed
-                </div>
-            </div>
-
-            <div className="story">
-                <div className="imagem">
-                    <img src="../assets/img/barked.svg" alt="barked"/>
-                </div>
-                <div className="usuario">
-                    barked
-                </div>
-            </div>
-
-            <div className="story">
-                <div className="imagem">
-                    <img src="../assets/img/nathanwpylestrangeplanet.svg" alt="nathanwpylestrangeplanet"/>
-                </div>
-                <div className="usuario">
-                    nathanwpylestrangeplanet
-                </div>
-            </div>
-
-            <div className="story">
-                <div className="imagem">
-                    <img src="../assets/img/wawawicomics.svg" alt="wawawicomics"/>
-                </div>
-                <div className="usuario">
-                    wawawicomics
-                </div>
-            </div>
-
-            <div className="story">
-                <div className="imagem">
-                    <img src="../assets/img/respondeai.svg" alt="respondeai"/>
-                </div>
-                <div className="usuario">
-                    respondeai
-                </div>
-            </div>
-
-            <div className="story">
-                <div className="imagem">
-                    <img src="../assets/img/filomoderna.svg" alt="filomoderna"/>
-                </div>
-                <div className="usuario">
-                    filomoderna
-                </div>
-            </div>
-
-            <div className="story">
-                <div className="imagem">
-                    <img src="../assets/img/memeriagourmet.svg" alt="memeriagourmet"/>
-                </div>
-                <div className="usuario">
-                    memeriagourmet
-                </div>
-            </div>
-
             <div className="setinha">
                 <ion-icon name="chevron-forward-circle"></ion-icon>
             </div>
